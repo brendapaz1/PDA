e5d20c90e2719378c2fa57508774b7254c38af1f
brendapaz1 <brendapaz1771@gmail.com>
2019-05-25 19:29:17 -0700

    holi

--- PDA/Program.cs
+++ PDA/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
-
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.Linq;
 namespace PDA
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] alphabet = { "a", "b", "c", "d"};
+            Console.WriteLine("Enter a string: ");
+            string chain = Console.ReadLine();
+            var inputChain = new Queue(SplitString(chain));
+            bool verify = VerifyChain(inputChain, alphabet);
+
         }
+
+        public static String[] SplitString(string chain)
+        {
+            string pattern = @"";
+            String[] charChain = Regex.Split(chain, pattern);
+            return charChain;
+                
+        }
+
+        public static bool VerifyChain(Queue input, string[] alphabet)
+        {
+            bool equal = alphabet.Intersect(input.ToArray()).Any(); 
+            return equal;
+        }
+
+
+
     }
 }
